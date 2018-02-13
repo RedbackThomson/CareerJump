04b368eec4d47c9e6b944c46c626cfba5787b41c
Redback <thomsonnicholas@hotmail.com>
2018-02-12 22:39:07 -0800

    Working Room Prototype

---
+++ public/js/room.js
@@ -0,0 +1,49 @@
+function addParticipantTracks(participant) {
+  participant.on('trackAdded', track => {
+    document.getElementById('remote-media-div').appendChild(track.attach());
+  });
+}
+
+function setupRoom(room) {
+  const localParticipant = room.localParticipant;
+  console.log('Connected to the Room as LocalParticipant "%s"', localParticipant.identity);
+  console.log('%s previously connected participants', room.participants.length);
+
+  // Log any Participants already connected to the Room
+  room.participants.forEach(addParticipantTracks);
+
+  room.on('participantConnected', function(participant) {
+    console.log('Participant "%s" has connected to the room', participant.identity);
+    addParticipantTracks(participant);
+  });
+
+  room.once('participantDisconnected', participant => {
+    console.log('Participant "%s" has disconnected from the room', participant.identity);
+    participant.tracks.forEach(track => {
+      track.detach().forEach(element => element.remove())
+    });
+  });
+
+  room.on('disconnected', room => {
+    // Detach the local media elements
+    room.localParticipant.tracks.forEach(track => {
+      var attachedElements = track.detach();
+      attachedElements.forEach(element => element.remove());
+    });
+  });
+}
+
+$.getJSON("/api/createRoom/" + roomName, function(data) {
+  let token = data.token;
+
+  Twilio.Video.connect(token, {
+    audio: true,
+    name: roomName,
+    video: { width: 640 }
+  }).then(setupRoom);
+});
+
+Twilio.Video.createLocalVideoTrack().then(track => {
+  var localMediaContainer = document.getElementById('local-media-ctr');
+  localMediaContainer.appendChild(track.attach());
+});
--- server.js
+++ server.js
@@ -12,14 +12,14 @@ app.engine('pug', require('pug').__express);
 app.use(express.static('public'));
 app.set('views', path.join(__dirname, '/views'));
 
-app.get('/createRoom/:roomName', (req, res) => {
+app.get('/api/createRoom/:roomName', (req, res) => {
   let twilioAccountSid = process.env.TWILIO_ACCOUNT_SID;
   let twilioApiKey = process.env.TWILIO_API_KEY;
   let twilioApiSecret = process.env.TWILIO_API_SECRET;
   // Create an access token which we will sign and return to the client,
   // containing the grant we just created
   const token = new AccessToken(twilioAccountSid, twilioApiKey, twilioApiSecret);
-  token.identity = 'careerjump';
+  token.identity = 'careerjump' + new Date().getTime();
 
   // Create a Video grant which enables a client to use Video 
   // and limits access to the specified Room (DailyStandup)
@@ -30,6 +30,13 @@ app.get('/createRoom/:roomName', (req, res) => {
   // Add the grant to the token
   token.addGrant(videoGrant);
   return res.json({token: token.toJwt()});
+});
+
+app.get('/room/:roomName', (req, res) => {
+  return res.render('room.pug', {
+    title: req.params.roomName,
+    roomName: req.params.roomName
+  });
 });
 
 app.get('/', (req, res) => res.render('index.pug'));
