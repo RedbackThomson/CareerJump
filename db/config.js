67ec04c688c4737d7509d46b75493109d3a878a8
Redback <thomsonnicholas@hotmail.com>
2018-03-05 18:36:39 -0800

    Added Sequelize Test Env

--- db/config.js
+++ db/config.js
@@ -4,6 +4,9 @@ module.exports = {
   'development': {
     'url': process.env.DATABASE_URL
   },
+  'test': {
+    'url': process.env.DATABASE_URL
+  },
   'production': {
     'url': process.env.DATABASE_URL
   }
