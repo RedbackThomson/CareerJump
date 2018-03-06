a26d939dd4301eee954644a019b69e575fe70fcb
Redback <thomsonnicholas@hotmail.com>
2018-03-05 19:27:35 -0800

    Required Postgres SSL

--- db/config.js
+++ db/config.js
@@ -3,14 +3,32 @@ require('dotenv').config();
 module.exports = {
   'development': {
     'url': process.env.DATABASE_URL,
-    'dialect': 'postgres'
+    'dialect': 'postgres',
+    'ssl': true,
+    'dialectOptions': {
+      'ssl': {
+        'require': true
+      }
+    }
   },
   'test': {
     'url': process.env.DATABASE_URL,
-    'dialect': 'postgres'
+    'dialect': 'postgres',
+    'ssl': true,
+    'dialectOptions': {
+      'ssl': {
+        'require': true
+      }
+    }
   },
   'production': {
     'url': process.env.DATABASE_URL,
-    'dialect': 'postgres'
+    'dialect': 'postgres',
+    'ssl': true,
+    'dialectOptions': {
+      'ssl': {
+        'require': true
+      }
+    }
   }
 };
