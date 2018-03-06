5ed5faf7c54f6acd6b3f52abd1f3f76d6a5d529a
Redback <thomsonnicholas@hotmail.com>
2018-03-05 19:29:49 -0800

    Removed Test SSL

--- db/config.js
+++ db/config.js
@@ -3,23 +3,11 @@ require('dotenv').config();
 module.exports = {
   'development': {
     'url': process.env.DATABASE_URL,
-    'dialect': 'postgres',
-    'ssl': true,
-    'dialectOptions': {
-      'ssl': {
-        'require': true
-      }
-    }
+    'dialect': 'postgres'
   },
   'test': {
     'url': process.env.DATABASE_URL,
-    'dialect': 'postgres',
-    'ssl': true,
-    'dialectOptions': {
-      'ssl': {
-        'require': true
-      }
-    }
+    'dialect': 'postgres'
   },
   'production': {
     'url': process.env.DATABASE_URL,
