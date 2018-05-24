0fea4f9bfef07d17b82331466a3ae8c665963b40
Redback <thomsonnicholas@hotmail.com>
2018-05-23 19:55:49 -0700

    Added Admin Interviewer List

--- src/routing/company/dashboard.js
+++ src/routing/company/dashboard.js
@@ -1,4 +1,17 @@
+getInterviewers = (models) => (req, res, next) => {
+  models.CompanyUser.findAll({
+    where: {companyId: req.user.company.id}
+  }).then(interviewers => {
+    req.interviewers = interviewers;
+    next();
+  }).catch(err => next(err));
+};
+
 module.exports = (models) =>
   (req, res) => {
-    return res.render('pages/dashboard/company', {interviews: req.interviews});
+    getInterviewers(models)(req, res, () => {
+      return res.render('pages/dashboard/company', {
+        interviewers: req.interviewers
+      });
+    });
   };
